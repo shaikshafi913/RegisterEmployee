e461db100643b6bb370dc959e4a300addfd5fdd7
Shafi <shaikshafi913@gmail.com>
2016-05-25 16:45:09 +0530

    User management entities created. Application properties configured.
    Signed-off-by: Shafi <shaikshafi913@gmail.com>

---
+++ DarkHorseManagement/src/main/java/com/darkhorse/DarkHorseManagementApplication.java
@@ -0,0 +1,12 @@
+package com.darkhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkHorseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkHorseManagementApplication.class, args);
+	}
+}
---
+++ DarkHorseManagement/src/main/java/com/darkhorse/Entities/User.java
@@ -0,0 +1,8 @@
+package com.darkhorse.Entities;
+
+import javax.persistence.Entity;
+
+@Entity	
+public class User {
+	
+}
